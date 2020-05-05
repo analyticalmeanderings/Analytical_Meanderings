8d93b97e35d1f37680ef10e70c770ef16eaa3034
mbc2010 <55868491+mbc2010@users.noreply.github.com>
2020-05-05 12:33:46 +0700

    heatmap done

--- feature_selection.py
+++ feature_selection.py
@@ -1,14 +1,15 @@
 # Importing all the libraries we need
 import pandas as pd
 import numpy as np
-import collections
-from sklearn.preprocessing import StandardScaler
-from sklearn.linear_model import LinearRegression
-from sklearn.neural_network import MLPClassifier
-import matplotlib.pyplot as plt
-import seaborn as sns
 import re
 import math
+import collections
+import matplotlib.pyplot as plt
+import seaborn as sns
+from sklearn.preprocessing import StandardScaler
+from sklearn.linear_model import LinearRegression
+from sklearn.feature_selection import SelectKBest, chi2
+from sklearn.neural_network import MLPClassifier
 
 # Some initializations to help our ETL phase
 player_data = pd.DataFrame()
@@ -19,7 +20,7 @@ index = 0
 num_players = 0
 position_counter = 0
 active_infront = 0
-stage = ['blinds','preflop', 'flop', 'turn', 'river']
+stage = ['blinds', 'preflop', 'flop', 'turn', 'river']
 stage_counter = 0
 pot_size = 0
 flop1 = ""
@@ -30,7 +31,7 @@ river = ""
 raw_hands = pd.DataFrame()
 
 # Computationaly intensive ETL code. The repository contains the output entitled "HANDS.csv"
-files = [str(item) for item in range(1,12)]
+files = [str(item) for item in range(1, 20)]
 
 for file in files:
     raw_hands = raw_hands.append(
@@ -56,7 +57,7 @@ for line in raw_hands.iloc[:, 0]:
             "Position": 100,
             "Card 1": '',
             "Card 2": '',
-            "Game ID": game_id,}, index=[0])
+            "Game ID": game_id, }, index=[0])
 
         player_data = player_data.append(current_player, ignore_index=True)
 
@@ -67,7 +68,8 @@ for line in raw_hands.iloc[:, 0]:
         hand.loc[index, 'Stage'] = stage[stage_counter]
         pot_size += hand.loc[index, 'Amount']
         hand.loc[index, 'Invested Post Action'] = hand.loc[index, 'Amount']
-        hand.loc[index, 'Starting Stack'] = player_data[player_data['Player Name'] == hand.loc[index, 'Player Name']]['Starting Stack'].sum()
+        hand.loc[index, 'Starting Stack'] = player_data[player_data['Player Name'] == hand.loc[index, 'Player Name']][
+            'Starting Stack'].sum()
         hand.loc[index, 'Invested Pre Action'] = 0
         hand.loc[index, 'Remaining Pre Action'] = hand.loc[index, 'Starting Stack']
 
@@ -99,19 +101,25 @@ for line in raw_hands.iloc[:, 0]:
             prior_action = 0
         hand.loc[index, 'Invested Pre Action'] = prior_action
 
-        hand.loc[index, 'Starting Stack'] = player_data[player_data['Player Name'] == line.split()[1]]['Starting Stack'].sum()
-        hand.loc[index, 'Remaining Pre Action'] = hand.loc[index, 'Starting Stack'] - hand.loc[index, 'Invested Pre Action']
+        hand.loc[index, 'Starting Stack'] = player_data[player_data['Player Name'] == line.split()[1]][
+            'Starting Stack'].sum()
+        hand.loc[index, 'Remaining Pre Action'] = hand.loc[index, 'Starting Stack'] - hand.loc[
+            index, 'Invested Pre Action']
         hand.loc[index, 'Pot Size'] = pot_size
         hand.loc[index, 'Amount to Call'] = hand['Invested Post Action'].max() - prior_action
 
         if 'folds' in line:
             hand.loc[index, 'Action'] = 'folds'
             hand.loc[index, 'Invested Post Action'] = hand.loc[index, 'Invested Pre Action']
+
         elif 'checks' in line or 'calls' in line:
             hand.loc[index, 'Action'] = 'calls'
-            hand.loc[index, 'Invested Post Action'] = hand.loc[index, 'Invested Pre Action'] + hand.loc[index, 'Amount to Call']
+            hand.loc[index, 'Invested Post Action'] = hand.loc[index, 'Invested Pre Action'] + hand.loc[
+                index, 'Amount to Call']
+
             if 'calls' in line:
                 active_infront += 1
+
         elif 'bets' in line or 'raises' in line:
             hand.loc[index, 'Action'] = 'raises'
             hand.loc[index, 'Amount'] = float(line.split('$')[-1].split()[0])
@@ -126,7 +134,6 @@ for line in raw_hands.iloc[:, 0]:
             pot_size += hand.loc[index, 'Invested Post Action'] - hand.loc[index, 'Invested Pre Action']
 
         index += 1
-
 
     elif '*** FLOP ***' in line:
         flop1 = line.split()[-1][:-1]
@@ -167,16 +174,16 @@ for line in raw_hands.iloc[:, 0]:
 
 hand_history = hand_history[hand_history['Action'] != 'small blind']
 hand_history = hand_history[hand_history['Action'] != 'big blind'].reset_index(drop=True)
-hand_history = hand_history[hand_history['Stage']=='preflop'].reset_index(drop=True)
+hand_history = hand_history[hand_history['Stage'] == 'preflop'].reset_index(drop=True)
 
 # This function breaks down a card string (e.g. Ac) into a distinct float value and a string for the suit
 def card_breakdown(df, card, value_col, suit_col):
-	df[value_col] = df[df[card] != ""][card].str[:-1]
-	card_values = {'T': 10, 'J': 11, 'Q': 12, 'K': 13, 'A': 14}
-	df[value_col].replace(card_values, inplace=True)
-	df[value_col] = df[value_col].astype('float')
-	df[suit_col] = df[df[card] != ""][card].str[-1]
-	return
+    df[value_col] = df[df[card] != ""][card].str[:-1]
+    card_values = {'T': 10, 'J': 11, 'Q': 12, 'K': 13, 'A': 14}
+    df[value_col].replace(card_values, inplace=True)
+    df[value_col] = df[value_col].astype('float')
+    df[suit_col] = df[df[card] != ""][card].str[-1]
+    return
 
 # Breaking down all the recorded card values
 card_breakdown(hand_history, 'Card 1', 'Value 1', 'Suit 1')
@@ -188,19 +195,22 @@ card_breakdown(hand_history, 'Turn', 'Value Turn', 'Suit Turn')
 card_breakdown(hand_history, 'River', 'Value River', 'Suit River')
 
 # descriptive metrics for suits
-def suitCounter(suit_string):
-	if len(suit_string) !=0:
-		return collections.Counter(suit_string).most_common(1)[0][1]
-	else:
-		return 0
+def suit_counter(suit_string):
+    if len(suit_string) != 0:
+        return collections.Counter(suit_string).most_common(1)[0][1]
+    else:
+        return 0
 
-hand_history['Common Suits'] = hand_history['Suit Flop1'].fillna('') + hand_history['Suit Flop2'].fillna('') + hand_history['Suit Flop3'].fillna('') + hand_history['Suit Turn'].fillna('') + hand_history['Suit River'].fillna('')
+hand_history['Common Suits'] = hand_history['Suit Flop1'].fillna('') + hand_history['Suit Flop2'].fillna('') + \
+                               hand_history['Suit Flop3'].fillna('') + hand_history['Suit Turn'].fillna('') + \
+                               hand_history['Suit River'].fillna('')
 hand_history['Player Suits'] = hand_history['Common Suits'] + hand_history['Suit 1'] + hand_history['Suit 2']
-hand_history['Common Suits'] = hand_history['Common Suits'].apply(suitCounter)
-hand_history['Player Suits'] = hand_history['Player Suits'].apply(suitCounter)
+hand_history['Common Suits'] = hand_history['Common Suits'].apply(suit_counter)
+hand_history['Player Suits'] = hand_history['Player Suits'].apply(suit_counter)
 hand_history['Raise Amount'] = hand_history['Amount'] - hand_history['Amount to Call']
 hand_history['Raise Amount'] = hand_history['Raise Amount'].fillna(0)
-features_pre = ['Value 1', 'Value 2', 'Player Suits', 'Position', 'Amount to Call', 'Pot Size', 'Active', 'Invested Pre Action', 'Starting Stack', 'Game ID']
+features_pre = ['Value 1', 'Value 2', 'Player Suits', 'Position', 'Amount to Call', 'Pot Size', 'Active',
+                'Invested Pre Action', 'Starting Stack', 'Game ID']
 
 hand_history['actions'] = hand_history['Action']
 hand_history = pd.get_dummies(hand_history, columns=['Action'])
@@ -216,9 +226,13 @@ mask = np.triu(np.ones_like(corr, dtype=np.bool))
 # Generate a custom diverging colormap
 cmap = sns.diverging_palette(220, 10, as_cmap=True)
 # Draw the heatmap with the mask and correct aspect ratio
-sns.heatmap(corr, mask=mask, cmap=cmap, vmax=.3, center=0,square=True, linewidths=.5, cbar_kws={"shrink": .5})
+sns.heatmap(corr, mask=mask, cmap=cmap, vmax=.3, center=0, square=True, linewidths=.5, cbar_kws={"shrink": .5})
 plt.tight_layout()
 plt.show()
+
+# Perform feature selection
+selector = SelectKBest(chi2, k=5)
+selector.fit(titanic[predictors], titanic["Survived"])
 
 # This code block scales and transforms features into normally distributed data
 # Without the normalization, our MLP algorithm throws errors
@@ -236,20 +250,20 @@ test_y = labels[highest_train_row:]
 lir = LinearRegression()
 lir.fit(train_x, train_y)
 predictions = lir.predict(test_x)
-lin_results = pd.DataFrame(predictions, columns = ['raises'])
+lin_results = pd.DataFrame(predictions, columns=['raises'])
 
-mlp = MLPClassifier(hidden_layer_sizes=(100,100), activation='logistic', max_iter=2000)
+mlp = MLPClassifier(hidden_layer_sizes=(100, 100), activation='logistic', max_iter=2000)
 mlp.fit(train_x, train_y.values.ravel())
 predictions = mlp.predict(test_x)
-ML_results = pd.DataFrame(predictions, columns = ['raises'])
+ML_results = pd.DataFrame(predictions, columns=['raises'])
 
 test_results = pd.DataFrame({
-    "Action":hand_history.loc[highest_train_row:,'actions'].reset_index(drop=True),
-    "ML_results":ML_results,
-    "lin_results":lin_results})
+    "Action": hand_history.loc[highest_train_row:, 'actions'].reset_index(drop=True),
+    "ML_results": ML_results,
+    "lin_results": lin_results})
 
-lin_acc = test_results[test_results['Action']==test_results['lin_results']].shape[0]/test_results.shape[0]
-ML_acc = test_results[test_results['Action']==test_results['ML_results']].shape[0]/test_results.shape[0]
+lin_acc = test_results[test_results['Action'] == test_results['lin_results']].shape[0] / test_results.shape[0]
+ML_acc = test_results[test_results['Action'] == test_results['ML_results']].shape[0] / test_results.shape[0]
 
 print(lin_acc)
 print(ML_acc)
